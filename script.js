da1029a9181a77abcf1d06f1420762f072c9c2d2
Alyeev <markmarkul123@gmail.com>
2021-01-07 23:07:45 +0400

    looping movies and put some conditions

--- script.js
+++ script.js
@@ -11,13 +11,29 @@ let personalMovieDB = {
 };
 
 
-const lastFilmOne = prompt('Baxdığınız ən son film', ''),
-      IMDB1 = prompt('Neçə qiymət verərdiz?', ''),
-      LastFilmTwo = prompt('Baxdığınız ən son film', ''),
-      IMDB2 = prompt('Neçə qiymət verərdiz?', '');
+for(let i = 1; i <= personalMovieDB.count; i++){
+  const lastFilmOne = prompt('Baxdığınız ən son film', ''),
+        IMDB1 = prompt('Neçə qiymət verərdiz?', '');
 
+  if(lastFilmOne != null && IMDB1 != null && lastFilmOne !='' && IMDB1 !='' && lastFilmOne.length<50){
+    personalMovieDB.movies[lastFilmOne] = IMDB1; //best practice is personalMovieDB.movies[lastFilmOne] because "." now working in every browser
+    console.log('Done');
+  }else{
+    console.log("error");
+    i--;
+  }
+}
 
-personalMovieDB.movies.lastFilmOne = IMDB1;
-personalMovieDB.movies.LastFilmTwo = IMDB2;
+if(personalMovieDB.count < 10){
+  console.log("Çox az filmə baxılıb");
+} else if( personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+  console.log('Həvəskarsız deyəsən');
+} else if(personalMovieDB.count >= 30){
+  console.log('Fanatsızki');
+} else{
+  console.log('Səhvlik baş verdi');
+}
 
 console.log(personalMovieDB);
+
+
